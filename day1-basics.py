bc2312e47390a4bf04e593194e43c4741649e3aa
haram-choudhry <harramchoudhry270@gmail.com>
2025-09-21 16:31:25 +0100

    day1-basics
    learned about variables and strings

---
+++ day1-basics.py
@@ -0,0 +1,21 @@
+# Day 1: Python Basics
+# 1. Strings
+name = "Harram"
+city = "London"
+print ("My name is", name, "and I live in", city,)
+# 2. Strings
+age = "19"
+year = "2025"
+print ("I am", age, "years old in", year,)
+# 3. Strings
+hobby1 = "reading"
+hobby2 = "photography"
+print ("I have two hobbies in total, one is", hobby1, "and the other is", hobby2,)
+# 4. Strings
+means_of_transportation_i_love = "TFL buses and London Underground"
+print ("My favourite means of transportation is", means_of_transportation_i_love,)
+current_read = "Ugly Love by Colleen Hoover"
+print (f"My current read is {current_read}")
+university_name = "BPP University"
+course_name = "BSc Applied Management"
+print (f"I study in {university_name} and my course name is {course_name}.")
